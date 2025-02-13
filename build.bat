fc133d170f25ec4a08ab601b0275e1423a1687f4
Pooyan <pooyan@altair.com>
2025-02-13 17:09:18 +0100

    moving compile files to build and bin

--- build.bat
+++ build.bat
@@ -1,6 +1,18 @@
 @echo off
+set build_dir=build
+set bin_dir=bin
+
+if not exist %build_dir% (
+    mkdir %build_dir%
+)
+
+if not exist %bin_dir% (
+    mkdir %bin_dir%
+)
+
+cd %build_dir%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
-cl /Ox /EHsc /std:c++17 /Zi main.cpp /Fe:test.exe
+cl /Ox /EHsc /std:c++17 /Zi ..\main.cpp /Fe:..\%bin_dir%\test.exe
 echo C++ Compilation complete.
-nvcc -o test_cuda.exe test_cuda.cu
+nvcc -o ..\%bin_dir%\test_cuda.exe ..\test_cuda.cu
 echo CUDA Compilation complete.
